820ba5b662a850afb569f154cb49e8c6fe83055e
Luke Bemish <lukebemish@gmail.com>
2022-02-06 18:46:38 -0700

    Update DynamicAssetGeneratorClient.java

--- common/src/main/java/dynamic_asset_generator/DynamicAssetGeneratorClient.java
+++ common/src/main/java/dynamic_asset_generator/DynamicAssetGeneratorClient.java
@@ -9,13 +9,13 @@ import net.minecraft.resources.ResourceLocation;
 public class DynamicAssetGeneratorClient {
     public static void init() {
         //testing
-
+        /*
         PaletteExtractor extractor = new PaletteExtractor(new ResourceLocation("minecraft","textures/block/stone.png"),
                 new ResourceLocation("minecraft","textures/block/diamond_ore.png"), 6);
         IPalettePlan plan = new ForegroundTransferType(extractor, new ResourceLocation("minecraft","textures/block/granite.png"),
                 true, false);
         DynAssetGeneratorClientAPI.planPaletteCombinedImage(new ResourceLocation("minecraft","textures/block/end_stone.png"), plan);
-
+        */
         /*
         DynAssetGenClientPlanner.planPaletteCombinedImage(new ResourceLocation("minecraft","textures/item/gold_ingot.png"),
                 new PlannedPaletteCombinedImage(new ResourceLocation("minecraft","textures/block/moss_block.png"), new ResourceLocation("dynamic_asset_generator","textures/empty.png"), new ResourceLocation("minecraft","textures/item/copper_ingot.png"), false, 6, true));
