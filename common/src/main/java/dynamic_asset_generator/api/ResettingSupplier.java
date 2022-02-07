ea733163896d4cc56a0696b3912172a1f908810d
Luke Bemish <lukebemish@gmail.com>
2022-02-06 22:45:06 -0600

    Add type fore ResettingSupplier

--- common/src/main/java/dynamic_asset_generator/api/ResettingSupplier.java
+++ common/src/main/java/dynamic_asset_generator/api/ResettingSupplier.java
@@ -2,6 +2,6 @@ package dynamic_asset_generator.api;
 
 import java.util.function.Supplier;
 
-public interface ResettingSupplier extends Supplier {
+public interface ResettingSupplier<T> extends Supplier<T> {
     public void reset();
 }
