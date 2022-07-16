ab7fc987a019c67fb2b4709ed559e4528f89714a
Luke Bemish <lukebemish@gmail.com>
2022-07-16 13:42:00 -0500

    Fix delegating pack resources on Quilt

--- Quilt/src/main/java/io/github/lukebemish/dynamic_asset_generator/quilt/ResourceDegrouper.java
+++ Quilt/src/main/java/io/github/lukebemish/dynamic_asset_generator/quilt/ResourceDegrouper.java
@@ -3,12 +3,20 @@ package io.github.lukebemish.dynamic_asset_generator.quilt;
 import com.google.auto.service.AutoService;
 import io.github.lukebemish.dynamic_asset_generator.impl.platform.services.IResourceDegrouper;
 import net.minecraft.server.packs.PackResources;
+import org.quiltmc.qsl.resource.loader.api.GroupResourcePack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AutoService(IResourceDegrouper.class)
 public class ResourceDegrouper implements IResourceDegrouper {
     public List<? extends PackResources> unpackPacks(List<? extends PackResources> packs) {
-        return packs;
+        ArrayList<PackResources> packsOut = new ArrayList<>();
+        packs.forEach(pack -> {
+            if (pack instanceof GroupResourcePack groupResourcePack) {
+                packsOut.addAll(groupResourcePack.getPacks());
+            } else packsOut.add(pack);
+        });
+        return packsOut;
     }
 }
