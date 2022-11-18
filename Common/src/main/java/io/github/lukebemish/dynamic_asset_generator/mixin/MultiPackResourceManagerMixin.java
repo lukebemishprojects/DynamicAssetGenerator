f1df3cd554ecdfeaba2e71c5e5bbf557daa9933a
Luke Bemish <lukebemish@gmail.com>
2022-11-18 17:17:40 -0600

    FIX: Make provided server resources properly load before tags

--- Common/src/main/java/io/github/lukebemish/dynamic_asset_generator/mixin/MultiPackResourceManagerMixin.java
+++ Common/src/main/java/io/github/lukebemish/dynamic_asset_generator/mixin/MultiPackResourceManagerMixin.java
@@ -7,22 +7,20 @@ import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.MultiPackResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.List;
 
 @Mixin(MultiPackResourceManager.class)
 public class MultiPackResourceManagerMixin {
-    @Inject(method = "<init>", at = @At(value = "RETURN"))
-    private void dynamic_asset_generator_insertResourcePack(PackType type,
-                                                            List<PackResources> packs,
-                                                            CallbackInfo cir) {
+    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"), argsOnly = true)
+    private static List<PackResources> dynamic_asset_generator$loadPacks(List<PackResources> packs, PackType type, List<PackResources> packsAgain) {
         if (type == PackType.CLIENT_RESOURCES) {
             ClientPrePackRepository.resetResources();
         }
         if (type == PackType.SERVER_DATA) {
             ServerPrePackRepository.loadResources(packs);
         }
+        return packs;
     }
 }
