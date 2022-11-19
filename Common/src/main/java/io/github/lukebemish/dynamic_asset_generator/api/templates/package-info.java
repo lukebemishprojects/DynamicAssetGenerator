1781f91beb458003346315cff1b6a6c8cf665023
Luke Bemish <lukebemish@gmail.com>
2022-11-18 23:02:52 -0600

    FEATURE: Initial work on new template system; added template codec and class for tag files.

---
+++ Common/src/main/java/io/github/lukebemish/dynamic_asset_generator/api/templates/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A series of classes and codecs to make parsing common data structures easier. These tools are especially useful for
+ * dynamically generated data, as often the vanilla tools to read certain data structures are not available at this
+ * time.
+ */
+package io.github.lukebemish.dynamic_asset_generator.api.templates;
