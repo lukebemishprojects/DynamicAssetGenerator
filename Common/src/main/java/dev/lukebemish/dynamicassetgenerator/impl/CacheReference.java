82519cc06e4ae19ed84e83e1ce83a2ec4565a70e
Luke Bemish <lukebemish@gmail.com>
2022-12-23 16:17:59 -0600

    CHORE: Update license headers

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/CacheReference.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/CacheReference.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2022 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package dev.lukebemish.dynamicassetgenerator.impl;
 
 import java.util.function.Consumer;
--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/util/package-info.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/util/package-info.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2022 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 @ApiStatus.Internal
 package dev.lukebemish.dynamicassetgenerator.impl.util;
 
