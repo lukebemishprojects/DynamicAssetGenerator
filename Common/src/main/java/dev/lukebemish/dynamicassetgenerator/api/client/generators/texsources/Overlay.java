dc1a2463e64755c6445c4ac6b3dc2b1f253a9fed
Luke Bemish <lukebemish@lukebemish.dev>
2023-03-26 13:31:30 -0500

    FIX: Fix Overlay sources with non-square images

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/Overlay.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/Overlay.java
@@ -44,49 +44,40 @@ public record Overlay(List<ITexSource> inputs) implements ITexSource {
             }
         }
         return () -> {
-            int maxX = 0;
-            int maxY = 0;
+            int maxWidth = 0;
+            int maxHeight = 0;
             List<NativeImage> images = new ArrayList<>();
             for (var input : inputs) {
                 images.add(input.get());
             }
             for (NativeImage image : images) {
-                if (image.getWidth() > maxX) {
-                    maxX = image.getWidth();
-                    maxY = image.getHeight();
+                if (image.getWidth() > maxWidth) {
+                    maxWidth = image.getWidth();
+                }
+            }
+            for (NativeImage image : images) {
+                int modifiedHeight = image.getHeight() * (maxWidth / image.getWidth());
+                if (modifiedHeight > maxHeight) {
+                    maxHeight = modifiedHeight;
                 }
             }
             try (MultiCloser ignored = new MultiCloser(images)) {
-                NativeImage output = NativeImageHelper.of(NativeImage.Format.RGBA, maxX, maxY, false);
+                NativeImage output = NativeImageHelper.of(NativeImage.Format.RGBA, maxWidth, maxHeight, false);
                 NativeImage base = images.get(0);
-                int xs;
-                int ys;
-                if (base.getWidth() / (base.getHeight() * 1.0) <= maxX / (maxY * 1.0)) {
-                    xs = maxX / base.getWidth();
-                    ys = maxY / base.getWidth();
-                } else {
-                    xs = maxX / base.getHeight();
-                    ys = maxY / base.getHeight();
-                }
-                for (int x = 0; x < maxX; x++) {
-                    for (int y = 0; y < maxY; y++) {
-                        output.setPixelRGBA(x, y, SafeImageExtraction.get(base, x / xs, y / ys));
+                int scale = maxWidth / base.getWidth();
+                for (int x = 0; x < maxWidth; x++) {
+                    for (int y = 0; y < maxHeight; y++) {
+                        output.setPixelRGBA(x, y, SafeImageExtraction.get(base, x / scale, y / scale));
                     }
                 }
                 if (images.size() >= 2) {
                     for (int i = 1; i < images.size(); i++) {
                         NativeImage image = images.get(i);
-                        if (image.getWidth() / (image.getHeight() * 1.0) <= maxX / (maxY * 1.0)) {
-                            xs = maxX / image.getWidth();
-                            ys = maxY / image.getWidth();
-                        } else {
-                            xs = maxX / image.getHeight();
-                            ys = maxY / image.getHeight();
-                        }
-                        for (int x = 0; x < maxX; x++) {
-                            for (int y = 0; y < maxY; y++) {
+                        scale = maxWidth / base.getWidth();
+                        for (int x = 0; x < maxWidth; x++) {
+                            for (int y = 0; y < maxHeight; y++) {
                                 ColorHolder input = ColorHolder.fromColorInt(SafeImageExtraction.get(output, x, y));
-                                ColorHolder top = ColorHolder.fromColorInt(SafeImageExtraction.get(image, x / xs, y / ys));
+                                ColorHolder top = ColorHolder.fromColorInt(SafeImageExtraction.get(image, x / scale, y / scale));
                                 ColorHolder outColor = ColorHolder.alphaBlend(top, input);
                                 output.setPixelRGBA(x, y, ColorHolder.toColorInt(outColor));
                             }
