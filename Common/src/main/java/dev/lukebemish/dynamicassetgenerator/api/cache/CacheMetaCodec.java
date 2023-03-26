dd42d2b7b97711c356432ae736198829818dcb7b
Luke Bemish <lukebemish@lukebemish.dev>
2023-03-25 20:01:50 -0500

    CHORE: Change license header plugin

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/cache/CacheMetaCodec.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/cache/CacheMetaCodec.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2022-2023 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package dev.lukebemish.dynamicassetgenerator.api.cache;
 
 import com.mojang.datafixers.util.Pair;
--- Common/src/main/resources/mods.groovy
+++ Common/src/main/resources/mods.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Luke Bemish and contributors
+ * Copyright (C) 2022-2023 Luke Bemish and contributors
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
