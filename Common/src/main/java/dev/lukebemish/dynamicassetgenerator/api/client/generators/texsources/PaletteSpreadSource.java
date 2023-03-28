b559182111849cd1ed209a103a12e6958345b510
Luke Bemish <lukebemish@lukebemish.dev>
2023-03-28 14:35:05 -0500

    FEATURE: Change PaletteSpreadSource behavior to allow for better defined ranges

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/PaletteSpreadSource.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/PaletteSpreadSource.java
@@ -5,8 +5,14 @@
 
 package dev.lukebemish.dynamicassetgenerator.api.client.generators.texsources;
 
+import java.util.List;
+import java.util.function.Function;
+
 import com.mojang.blaze3d.platform.NativeImage;
+
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.lukebemish.dynamicassetgenerator.api.ResourceGenerationContext;
 import dev.lukebemish.dynamicassetgenerator.api.client.generators.ITexSource;
@@ -17,13 +23,62 @@ import dev.lukebemish.dynamicassetgenerator.impl.client.palette.Palette;
 import net.minecraft.server.packs.resources.IoSupplier;
 import org.jetbrains.annotations.Nullable;
 
-public record PaletteSpreadSource(ITexSource source, float paletteCutoff, float lowerBound, float upperBound) implements ITexSource {
+public record PaletteSpreadSource(ITexSource source, float paletteCutoff, List<Range> range) implements ITexSource {
     public static final Codec<PaletteSpreadSource> CODEC = RecordCodecBuilder.create(i -> i.group(
             ITexSource.CODEC.fieldOf("source").forGetter(PaletteSpreadSource::source),
             Codec.FLOAT.optionalFieldOf("palette_cutoff", Palette.DEFAULT_CUTOFF).forGetter(PaletteSpreadSource::paletteCutoff),
-            Codec.FLOAT.optionalFieldOf("lower_bound",0f).forGetter(PaletteSpreadSource::lowerBound),
-            Codec.FLOAT.optionalFieldOf("upper_bound",1f).forGetter(PaletteSpreadSource::upperBound)
+            Codec.either(Range.CODEC, Range.CODEC.listOf()).xmap(
+                    either -> either.map(List::of, Function.identity()),
+                    list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list)
+            ).flatXmap(list -> {
+                if (!verifyDisjoint(list)) {
+                    return DataResult.error(() -> "Ranges must be disjoint");
+                }
+                return DataResult.success(list);
+            }, DataResult::success).optionalFieldOf("range", List.of(new Range(0.0f,1.0f))).forGetter(PaletteSpreadSource::range)
     ).apply(i,PaletteSpreadSource::new));
+
+    public record Range(float lowerBound, float upperBound) {
+        public static final Codec<Range> CODEC = Codec.floatRange(0.0f,1.0f).listOf().flatXmap(list -> {
+            if (list.size() != 2) {
+                return DataResult.error(() -> "Range must have exactly 2 elements");
+            }
+            return DataResult.success(new Range(list.get(0), list.get(1)));
+        }, range -> DataResult.success(List.of(range.lowerBound(), range.upperBound())));
+    }
+
+    static boolean verifyDisjoint(List<Range> ranges) {
+        if (ranges.size() == 0) {
+            return false;
+        }
+        for (int i = 0; i < ranges.size(); i++) {
+            if (ranges.get(i).lowerBound() > ranges.get(i).upperBound()) {
+                return false;
+            }
+            for (int j = i+1; j < ranges.size() && j < i+2; j++) {
+                if (ranges.get(i).upperBound() > ranges.get(j).lowerBound()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static float mapToRange(float value, List<Range> ranges) {
+        float sum = 0.0f;
+        for (Range range : ranges) {
+            sum += range.upperBound()-range.lowerBound();
+        }
+        float current = 0.0f;
+        for (Range range : ranges) {
+            float rangeSize = range.upperBound()-range.lowerBound();
+            if (value < current+rangeSize) {
+                return range.lowerBound()+(value-current)*(rangeSize/sum);
+            }
+            current += rangeSize;
+        }
+        return ranges.get(ranges.size()-1).upperBound();
+    }
 
     @Override
     public Codec<? extends ITexSource> codec() {
@@ -43,7 +98,6 @@ public record PaletteSpreadSource(ITexSource source, float paletteCutoff, float 
                 NativeImage outImg = NativeImageHelper.of(NativeImage.Format.RGBA, sourceImg.getWidth(), sourceImg.getHeight(), false);
 
                 int maxIndex = palette.getSize()-1;
-                float diff = upperBound()-lowerBound();
 
                 for (int x = 0; x < sourceImg.getWidth(); x++) {
                     for (int y = 0; y < sourceImg.getHeight(); y++) {
@@ -53,7 +107,7 @@ public record PaletteSpreadSource(ITexSource source, float paletteCutoff, float 
                             continue;
                         }
                         float ramp = ((float) palette.closestTo(original))/maxIndex;
-                        float value = lowerBound()+(diff*ramp);
+                        float value = mapToRange(ramp, range());
                         outImg.setPixelRGBA(x,y,ColorHolder.toColorInt(new ColorHolder(value,value,value,original.getA())));
                     }
                 }
