7b933f2e9edb43c8cd8196fc82d38990dee84e7e
Luke Bemish <lukebemish@lukebemish.dev>
2023-05-31 19:54:08 -0600

    Fix license headers

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/AbstractManyOperationSource.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/api/client/generators/texsources/AbstractManyOperationSource.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package dev.lukebemish.dynamicassetgenerator.api.client.generators.texsources;
 
 import com.mojang.blaze3d.platform.NativeImage;
