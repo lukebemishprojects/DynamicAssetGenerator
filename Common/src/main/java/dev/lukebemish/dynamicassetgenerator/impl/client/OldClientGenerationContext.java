207bb4e0368d5aaecd2ed4a4c7cc5d24f3a79e28
Luke Bemish <lukebemish@lukebemish.dev>
2023-06-21 22:06:37 -0400

    Fix license headers

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/client/OldClientGenerationContext.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/client/OldClientGenerationContext.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package dev.lukebemish.dynamicassetgenerator.impl.client;
 
 import dev.lukebemish.dynamicassetgenerator.api.client.ClientPrePackRepository;
