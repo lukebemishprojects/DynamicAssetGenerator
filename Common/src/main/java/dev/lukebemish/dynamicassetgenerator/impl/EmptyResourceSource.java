e3099741197cc771c742a9d48b978c0a99ccc2a9
Luke Bemish <lukebemish@lukebemish.dev>
2023-08-06 16:41:01 -0400

    Fix licenses

--- Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/EmptyResourceSource.java
+++ Common/src/main/java/dev/lukebemish/dynamicassetgenerator/impl/EmptyResourceSource.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package dev.lukebemish.dynamicassetgenerator.impl;
 
 import dev.lukebemish.dynamicassetgenerator.api.ResourceGenerationContext;
